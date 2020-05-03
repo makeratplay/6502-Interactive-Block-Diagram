bf76feeac2629520535cd12711e3554570ffce67
Michael Hawkins <makeratplay@gmail.com>
2020-05-03 14:35:08 -0500

    Got to start somewheres

---
+++ app.js
@@ -0,0 +1,112 @@
+
+var controlLines = [
+
+   
+    { Name: "#DAA", left: 	613	, top: 909 },
+    { Name: "#DAA", left: 583, top:  1398 },
+    { Name: "#DAA", left: 865, top: 1395 },
+    { Name: "#DSA", left: 552, top: 1293 },
+    { Name: "#DSA", left: 898, top: 1298 },
+    { Name: "0/ADH(1-7)", left: 	1133	, top: 340 },
+    { Name: "0/ADH0", left: 	1133	, top: 318 },
+    { Name: "0/ADL0", left: 	583	, top: 594 },
+    { Name: "0/ADL1", left: 	583	, top: 610 },
+    { Name: "0/ADL2", left: 	583	, top: 626 },
+    { Name: "0ADD", left: 	637	, top: 1249 },
+    { Name: "AC/DB", left: 	621	, top: 1496 },
+    { Name: "AC/SB", left: 	868	, top: 1493 },
+    { Name: "ACR", left: 	379	, top: 1231 },
+    { Name: "ACR", left: 	628	, top: 1150 },
+    { Name: "ACR/C", left: 	265	, top: 1300 },
+    { Name: "ADD/SB(0-6)", left: 	933	, top: 958 },
+    { Name: "ADD/SB(7)", left: 	933	, top: 1065 },
+    { Name: "ADDADL", left: 	910	, top: 925 },
+    { Name: "ADH/ABH", left: 	1032	, top: 389 },
+    { Name: "ADHPCH", left: 	917	, top: 267 },
+    { Name: "ADL/ABL", left: 	1044	, top: 803 },
+    { Name: "ADL/ADD", left: 	716	, top: 817 },
+    { Name: "ADLPCL", left: 	688	, top: 267 },
+    { Name: "ANDS", left: 	735	, top: 1005 },
+    { Name: "AVR", left: 	401	, top: 1249 },
+    { Name: "AVR", left: 	597	, top: 1157 },
+    { Name: "AVR/V", left: 	265	, top: 1494 },
+    { Name: "DB/ADD", left: 	558	, top: 758 },
+    { Name: "DB0/C", left: 	265	, top: 1279 },
+    { Name: "DB1/Z", left: 	265	, top: 1324 },
+    { Name: "DB2/I", left: 	265	, top: 1359 },
+    { Name: "DB3/D", left: 	265	, top: 1391 },
+    { Name: "DB6/V", left: 	265	, top: 1469 },
+    { Name: "DB7/N", left: 	265	, top: 1518 },
+    { Name: "DBADD", left: 	650	, top: 803 },
+    { Name: "DBZ", left: 	442	, top: 1288 },
+    { Name: "DBZ/Z", left: 	265	, top: 1339 },
+    { Name: "DL/ADH", left: 	948	, top: 214 },
+    { Name: "DL/ADL", left: 	773	, top: 214 },
+    { Name: "DL/DB", left: 	542	, top: 214 },
+    { Name: "EORS", left: 	735	, top: 1041 },
+    { Name: "HC", left: 	661	, top: 1146 },
+    { Name: "I/ADDC", left: 	680	, top: 913 },
+    { Name: "I/PC", left: 	577	, top: 345 },
+    { Name: "I/V", left: 	265	, top: 1495 },
+    { Name: "IR5", left: 	360	, top: 1246 },
+    { Name: "IR5/C", left: 	265	, top: 1290 },
+    { Name: "IR5/D", left: 	265	, top: 4101 },
+    { Name: "IR5/I", left: 	265	, top: 1370 },
+    { Name: "ORS", left: 	735	, top: 1078 },
+    { Name: "P/DB", left: 	458	, top: 1498 },
+    { Name: "PCHADH", left: 	942	, top: 579 },
+    { Name: "PCH/DB", left: 	785	, top: 578 },
+    { Name: "PCH/PCH", left: 	819	, top: 267 },
+    { Name: "PCHC", left: 	867	, top: 370 },
+    { Name: "PCLADL", left: 	710	, top: 520 },
+    { Name: "PCL/DB", left: 	554	, top: 519 },
+    { Name: "PCL/PCL", left: 	583	, top: 267 },
+    { Name: "PCLC", left: 	685	, top: 394 },
+    { Name: "PCLC", left: 	922	, top: 373 },
+    { Name: "phi1", left: 	256	, top: 1211 },
+    { Name: "phi1", left: 	462	, top: 345 },
+    { Name: "phi2", left: 	1105	, top: 256 },
+    { Name: "phi2", left: 	256	, top: 1231 },
+    { Name: "phi2", left: 	315	, top: 166 },
+    { Name: "phi2", left: 	369	, top: 465 },
+    { Name: "phi2", left: 	496	, top: 166 },
+    { Name: "phi2", left: 	592	, top: 727 },
+    { Name: "phi2", left: 	595	, top: 418 },
+    { Name: "phi2", left: 	774	, top: 863 },
+    { Name: "phi2", left: 	786	, top: 1108 },
+    { Name: "phi2", left: 	905	, top: 482 },
+    { Name: "S/ADL", left: 	777	, top: 809 },
+    { Name: "S/SB", left: 	946	, top: 813 },
+    { Name: "SB/AC", left: 	801	, top: 1408 },
+    { Name: "SBADD", left: 	769	, top: 1219 },
+    { Name: "SB/ADH", left: 	878	, top: 595 },
+    { Name: "SB/DB", left: 	849	, top: 631 },
+    { Name: "SB/S", left: 	943	, top: 708 },
+    { Name: "SB/X", left: 	1010	, top: 1089 },
+    { Name: "SB/Y", left: 	1013	, top: 1320 },
+    { Name: "SRS", left: 	735	, top: 1116 },
+    { Name: "SS", left: 	830	, top: 711 },
+    { Name: "SUMS", left: 	735	, top: 968 },
+    { Name: "T1", left: 	269	, top: 426 },
+    { Name: "X/SB", left: 	1008	, top: 1198 },
+    { Name: "Y/SB", left: 	1013	, top: 1440 },
+
+    
+
+]
+
+
+
+function mapIt(){
+    $('#blockDiagram').empty();
+    let data = $("#ControlSignals"). val();
+    controlLines.forEach( item => {
+        
+        if ( data.includes( item.Name ) ){
+            let title = item.Name + "( " + item.left + ", " + item.top + " )";
+            let div = '<div class="highlight" style="top: ' + item.top + 'px; left: ' + item.left + 'px;" title = "' + title  +'" ></div>'
+            $('#blockDiagram').append(div);
+        }
+    })
+    
+}
